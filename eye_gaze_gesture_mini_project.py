2a67e752d5f159cadc341c453839356f1569a45e
Kongskrald <pcsprog@hotmail.com>
2018-11-01 11:30:03 +0100

    integracted the eye tracker and leap motion into eye_gaze_gesture_mini_project.py. It now works more or less but is still a long way to being usable by a user.

--- eye_gaze_gesture_mini_project.py
+++ eye_gaze_gesture_mini_project.py
@@ -1,7 +1,7 @@
 """
 Desciprtion:
-    This program provides a simulated mouse interface using
-    eye gaze and a gesture recognition. 
+    This program uses the tobii eye tracker and leap motion to get the users gaze and gestures being made. Currently it
+    can only drag a window when the user makes a fist.
     
 Authors:
     Shagen Djanian, Aalborg University
@@ -9,64 +9,193 @@ Authors:
 """
 
 import pyautogui
-import keyboard
+import keyboard  
 pyautogui.FAILSAFE = True # Drag mouse to upper left corner to trigger failsafe
 import win32gui
 import gesture_reader 
+import tobii_research as tr
+
+from pygaze.display import Display
+from pygaze.eyetracker import EyeTracker
+import time
+import sys
+from pynput.mouse import Button, Controller
+import numpy as np
+import pandas as pd
+
+
+global mouse
+mouse = Controller()
+
+
+def calibrateEyeTrackerPyGaze():
+    """
+        Runs the callibration process using PyGaze and tobii eye trackers.
+    """
+
+    disp = Display()
+    tracker = EyeTracker(disp,resolution=(1600,900),  trackertype='tobii')
+    
+    tracker.calibrate()
+    
+    #tracker.close() 
+    disp.close()
+    return tracker
+
+
+
+def avaliableEyeTrackers():
+    """
+        Checks for the available eye trackers connected using tobii SDK
+    """
+# <BeginExample>
+    eyetrackers = tr.find_all_eyetrackers()
+
+    for eyetracker in eyetrackers:
+        print("Address: " + eyetracker.address)
+        print("Model: " + eyetracker.model)
+        print("Name (It's OK if this is empty): " + eyetracker.device_name)
+        print("Serial number: " + eyetracker.serial_number)
+    # <EndExample>
+    return eyetrackers
 
 
 def grabObject(xy_pos = None):
+    """
+        Clicks on the current position to get the window to be the window in the foreground
+        and returns that windows handle
+    """
     #pyautogui.moveTo(xy_pos)
     #pyautogui.mouseDown(button='left')
-    pyautogui.click();
+    #pyautogui.click();
+    mouse.press(Button.left)
+    mouse.release(Button.left)
     current_window = win32gui.GetForegroundWindow()
     return current_window
 
 
 def moveWin32Gui(pos, hwnd=None):
-        win_size,offset,size = getOffset(pos, hwnd)
-        x_pos = pos[0]
-        y_pos = pos[1]
-        x_offset = offset[0]
-        y_offset = offset[1]   
-        w_window = size[0]
-        h_window = size[1]
-        win32gui.MoveWindow(hwnd,x_pos+x_offset,y_pos+y_offset,w_window,h_window, False)
-        #print('Moving to',current_position)
+    
+    """
+        Moves the window by using the Win32Gui. This produces a bug where the window is flashing because it is
+        being put on top of the window stack all the time. Only works with windows.
+    
+    """
+    pos = (int(pos[0]),int(pos[1]))
+    win_size,offset,size = getOffset(pos, hwnd)
+    x_pos = pos[0]
+    y_pos = pos[1]
+    x_offset = offset[0]
+    y_offset = offset[1]   
+    w_window = size[0]
+    h_window = size[1]
+    win32gui.MoveWindow(hwnd,x_pos+x_offset,y_pos+y_offset,w_window,h_window, False)
+    #print('Moving to',current_position)
         
 def movePyAutoGui(pos, hwnd=None):
-        win_size,offset,size = getOffset(pos,hwnd)
-        x_pos = pos[0]
-        y_pos = pos[1]
-        x_offset = offset[0]
-        y_offset = offset[1]   
-        pyautogui.moveTo((x_pos-x_offset+150,y_pos-y_offset+10))
-        pyautogui.mouseDown(button='left')
-        #print('Moving to',current_position)
+    """
+        Moves the window using PyAutoGui. This is platform independents, but PyAutoGui bugs when used with PyGaze, so
+        until that bug is removed it is unusable.
+    """
+    #pos = (int(pos[0]),int(pos[1]))
+    win_size,offset,size = getOffset(pos,hwnd)
+    x_pos = pos[0]
+    y_pos = pos[1]   
+    x_offset = offset[0]
+    y_offset = offset[1]   
+    #mouse.release(Button.left)
+    pyautogui.moveTo((x_pos-x_offset+size[1]/2,y_pos-y_offset+10))
+    pyautogui.mouseDown(button='left')
+    print('old pos',pos[0],pos[1])
+    print('new x and y pos to move to',x_pos-x_offset+size[1]/2,y_pos-y_offset+10)
+    #mouse.position = ((x_pos-x_offset+size[1]/2,y_pos-y_offset+10))
+    #mouse.position = ((x_pos,y_pos))
+    #mouse.press(Button.left)
+        
+def movePynput(pos, hwnd=None):
+    """
+        Moves the window using Pynput. This is the current implementation as is does not bug when used with PyGaze
+    """
+    x_pos = pos[0]
+    y_pos = pos[1]
+    print('position: ',pos[0],pos[1])
+    #print('new x and y pos to move to',x_pos-x_offset+size[1]/2,y_pos-y_offset+10)
+    #mouse.position = ((x_pos-x_offset+size[1]/2,y_pos-y_offset+10))
+    mouse.position = ((x_pos,y_pos))
+    mouse.press(Button.left)
+
+def shift1(arr,num = 1):
+    """
+    Shifts the array by 1 converting array to panda, shifting and converting to list. 
+    """
+    data = pd.Series(arr)
+    data = data.shift(num)
+    return data.tolist()
+
+def shift2(arr,num = 1):
+    """
+    Shifs the array by 1 using np.roll.
+    """
+    arr=np.roll(arr,num)
+    if num<0:
+         np.put(arr,range(len(arr)+num,len(arr)),np.nan)
+    elif num > 0: 
+         np.put(arr,range(num),np.nan)
+    return arr
+
+def updateRollingArray(arr,new_value):
+    """
+    Shifts the array and puts the new value in the first index's place
+    """
+    shifted_arr = shift1(arr)
+    shifted_arr[0] = new_value
+    return shifted_arr
+
+    
+
         
 def getOffset(pos,hwnd = None):
-        win_size = win32gui.GetWindowRect(hwnd)
-        for i in win_size:
-            print(i)
-        x_pos = pos[0]
-        y_pos = pos[1]
-        x_window = win_size[0]
-        y_window = win_size[1]
-        w_window = win_size[2] - x_window
-        h_window = win_size[3] - y_window
-        x_offset = x_pos - x_window
-        y_offset = y_pos - y_window
+    """
+    Calculates the offset from the mouse to the top left corner of a window.
+    
+    input:
+        pos = a tuple of x and y coordinates of the current position
         
-        offset=(x_offset,y_offset)
-        size = (h_window,w_window)
-        return win_size,offset,size
+    return:
+        win_size = top left corner and bottom right corner of a window using Win32Gui
+        offset = The x and y offset from the mouse to the window corner
+        size = height and width of the window
+        
+    """
+    win_size = win32gui.GetWindowRect(hwnd)
+    #for i in win_size:
+    #    print(i)
+    x_pos = pos[0]
+    y_pos = pos[1]
+    x_window = win_size[0]
+    y_window = win_size[1]
+    w_window = win_size[2] - x_window
+    h_window = win_size[3] - y_window
+    x_offset = x_pos - x_window
+    y_offset = y_pos - y_window
+    
+    offset=(x_offset,y_offset)
+    size = (h_window,w_window)
+    return win_size,offset,size
             
 def currentGazePosition():
-    current_position = pyautogui.position()
+    """
+        Emulates a gaze position by simply getting the current mouse position. 
+    """
+    #current_position = pyautogui.position()
+    current_position = mouse.position
     return current_position
 
     
 def grabGesture(grabBool = False):
+    """
+        An function used during develpment that no longer is relevan
+    """
     if keyboard.is_pressed('q'):
         grabBool = True
     return grabBool
@@ -77,9 +206,14 @@ if __name__ == "__main__":
     listener = gesture_reader.SampleListener()
     controller = gesture_reader.Leap.Controller()
 
-    # Have the sample listener receive events from the controller
+    # Have the sample listener receive events  from the controller
     controller.add_listener(listener)    
     #pygame.mouse.set_visible(True)
+    eyetracker = calibrateEyeTrackerPyGaze()
+    eyetracker.start_recording()
+    #gaze_array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    gaze_array = range(1,30)
+
     
     ## Loop
     print('Press Ctrl-C to quit.')
@@ -88,18 +222,33 @@ if __name__ == "__main__":
         grab_bool = False;
         while True:
             fistStatus = listener.getFist(controller)
-            print(fistStatus)
-            current_position = currentGazePosition()
+            print('fist status is: ',fistStatus)
+            #print('current gaze is at: ',current_gaze_sample)
+            current_sample = eyetracker.sample()
+            gaze_array = updateRollingArray(gaze_array,current_sample)
+
+            print('Gaze is at:  ',current_sample)          
+            #current_position = currentGazePosition()
             if (fistStatus == True):
                current_window_handle = grabObject()
                while (fistStatus==True):
+                   print('fist status is: ',fistStatus)
+                   #current_position = currentGazePosition()
+                   current_sample = eyetracker.sample()
+                   gaze_array = updateRollingArray(gaze_array,current_sample)
+                   current_gaze_mean = np.mean(gaze_array,axis=0)
+                   print('Gaze mean is at:  ',current_gaze_mean)
+                   movePynput(current_gaze_mean,current_window_handle)
                    fistStatus = listener.getFist(controller) # Update the fistStatus in the while loop
-                   current_position = currentGazePosition()
-                   movePyAutoGui(current_position,current_window_handle)
+
             if(fistStatus == False):
-                pyautogui.mouseUp(button='left')
+                #pyautogui.mouseUp(button='left')
+                mousePressCheck=False
+                mouse.release(Button.left)
                   
              
     except KeyboardInterrupt:
         print('\n Interrupted.')
+        eyetracker.stop_recording()
+        eyetracker.close()
     
--- gesture_reader.py
+++ gesture_reader.py
@@ -9,7 +9,18 @@
 Desciprtion:
     This script provides an interface to recognize gestures using Leap Motion.
     To install the Python SDK follow https://developer-archive.leapmotion.com/documentation/python/index.html .
-    It only runs on Python 2.7.
+    It only runs on Python 2.7. It currently on recognizes a fist by looking at the angles between the fingers.
+    An example of how to use it:
+            # Create a sample listener and controller
+            listener = SampleListener()
+            controller = Leap.Controller()
+
+            # Have the sample listener receive events from the controller
+            controller.add_listener(listener)
+            
+            # Check if a fist is currently being made
+            fiststatus = listener.getFist(controller)
+            print(fiststatus)
     
     Current recognized gestures:
         Fist - getFist(Leap.controller)
@@ -18,6 +29,7 @@ Authors:
     Shagen Djanian, Aalborg University
     Marike van den Broek, Aalborg University
 """
+## Finds the relative path to the correct library needed for Leap.py
 import os, sys, inspect
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
@@ -25,17 +37,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
 
 import Leap
-
-import thread, time
 import numpy as np
 import numpy.linalg as la
-import copy
-
+import itertools
+import operator
+import math
+import pandas as pd
 
 class SampleListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
     bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
-
+    fiveFistArray = [False,False,False,False,False]
     def on_init(self, controller):
         print("Initialized")
 
@@ -49,6 +61,57 @@ class SampleListener(Leap.Listener):
     def on_exit(self, controller):
         print ("Exited")
         
+    def shift2(self,arr,num = 1):
+        """
+        Shifs the array by 1 using np.roll.
+        """
+        arr=np.roll(arr,num)
+        if num<0:
+             np.put(arr,range(len(arr)+num,len(arr)),np.nan)
+        elif num > 0:
+             np.put(arr,range(num),np.nan)
+        return arr
+
+    def shift1(self,arr,num = 1):
+        """
+        Shifts the array by 1 converting array to panda, shifting and converting to list. 
+        """
+        data = pd.Series(arr)
+        data = data.shift(num)
+        return data.tolist()
+
+
+    def most_common(L):
+      """
+          Gets the most common occurence in a list. Code from https://stackoverflow.com/questions/1518522/find-the-most-common-element-in-a-list/20872750
+      """
+      # get an iterable of (item, iterable) pairs
+      SL = sorted((x, i) for i, x in enumerate(L))
+      # print 'SL:', SL
+      groups = itertools.groupby(SL, key=operator.itemgetter(0))
+      # auxiliary function to get "quality" for an item
+      def _auxfun(g):
+        item, iterable = g
+        count = 0
+        min_index = len(L)
+        for _, where in iterable:
+          count += 1
+          min_index = min(min_index, where)
+        # print 'item %r, count %r, minind %r' % (item, count, min_index)
+        return count, -min_index
+      # pick the highest-count/earliest item
+      return max(groups, key=_auxfun)[0]          
+
+      # get an iterable of (item, iterable) pairs
+ 
+
+    def updateRollingArray(self,arr,new_value):
+        """
+        Shifts the array and puts the new value in the first index's place
+        """
+        shifted_arr = self.shift2(arr)
+        shifted_arr[0] = new_value
+        return shifted_arr
 
     def unit_vector(self,vector):
         """ Returns the unit vector of the vector.  """
@@ -56,7 +119,6 @@ class SampleListener(Leap.Listener):
 
     def angle_between(self,v1, v2):
         """ Returns the angle in radians between vectors 'v1' and 'v2'::
-    
                 >>> angle_between((1, 0, 0), (0, 1, 0))
                 1.5707963267948966
                 >>> angle_between((1, 0, 0), (1, 0, 0))
@@ -124,9 +186,21 @@ class SampleListener(Leap.Listener):
             fist = False
         return fist
 
+    def getMajorityBool(self,arr):
+        """
+            Counts how many trues are in a boolean array. If there are more trues than false, returns true.
+        """
+        #num_true = arr.count(True)
+        num_true = sum(arr)
+        if num_true > math.ceil(len(arr)/2.0):
+            return True
+        else:
+            return False
+
     def getFist(self, controller):
         """
-        Get the status of a fist. If no hand is detected it prints no hands.
+        Get the status of a fist. If no hand is detected it prints no hands. Is uses a rolling mean, so if the 5 most
+        recent samples for a fist are true it returns true.
         """
         
         angle_array = self.get_current_angles(controller)
@@ -136,6 +210,8 @@ class SampleListener(Leap.Listener):
                 return
         else:
             fist_status = self.checkFistGesture(angle_array)
+            self.fiveFistArray = self.updateRollingArray(self.fiveFistArray,fist_status)
+            fist_status = self.getMajorityBool(self.fiveFistArray)
             return fist_status
         
     def LeapVectorToNpArray(self,leap_vec):
@@ -147,6 +223,9 @@ class SampleListener(Leap.Listener):
         return nparray
     
     def on_frame(self, controller):
+        '''
+        Sample code from leap motion. Uncomment all print all values you receive from a hand.
+        '''
         # Get the most recent frame and report some basic information
         frame = controller.frame()
 
